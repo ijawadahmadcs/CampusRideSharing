5933b3e0ff2467c286bacd9031c4fd7e60b861c4
ijawadahmadcs <ijawadahmadcs@gmail.com>
2025-11-26 20:36:30 +0500

    final

--- management/DatabaseConfig.java
+++ management/DatabaseConfig.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/ridesharedb";
     private static final String USER = "root";
-    private static final String PASSWORD = "Jawadahmad@3612";
+    private static final String PASSWORD = "Pakistan@2025";
     
     private static Connection connection = null;
     
